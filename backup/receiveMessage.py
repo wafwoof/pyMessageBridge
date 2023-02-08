7435283f18cca197e8f60478249c24f09323cef5
wafwoof <kazmcquaid@gmail.com>
2023-02-08 11:59:23 -0800

    Deleted backup applescripts dir

--- backup/receiveMessage.py
+++
@@ -1,8 +0,0 @@
-import sys
-import requests
-
-url = 'https://rdmap.dev/chat/submit'
-
-for line in sys.stdin:
-    response = requests.post(url, json = {'username': 'iMessage-Forward', 'content': line})
-    print(response)
