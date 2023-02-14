0ed9bafa8ec9642a01e37f2c5e050accb18e8432
wafwoof <kazmcquaid@gmail.com>
2023-02-14 01:26:21 -0800

    Fixed small setup gaps.

--- run-pyMessageBridge+noJared.sh
+++ run-pyMessageBridge+noJared.sh
@@ -1,7 +1,7 @@
 echo "Running pyMessageBridge + noJared..."
 echo ""
 echo "This may ask for your credentials to kill port 8000 (the first time)."
-echo "If you are alarmed; feel free to check the commands in this script."
+echo "If you are alarmed; feel free to check line 5 in this script."
 sudo lsof -t -i tcp:8000 | xargs kill -9
 ps -a | grep noJared | awk '{print $1}' | xargs kill -9
 
--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+# pyMessageServer setup script
+
 import json
+
+# get input from user
+print("\n######################################")
+print("#### pyMessageServer setup script ####")
+print("######################################")
+techphoneNumber = input("Please enter your techphone number (Format: +15555555555): ")
 
 # create template config file
 with open('server.config.json', 'w') as config_file:
     json.dump({
-        "techphoneNumber": "+15555555555",
+        "techphoneNumber": techphoneNumber,
         "managerWhitelist": [],
         "textCommandSymbol": "¥",
         "forwardOutgoingMessages": False
@@ -13,3 +21,7 @@ with open('server.config.json', 'w') as config_file:
 # create empty log.txt
 with open('log.txt', 'w') as log_file:
     log_file.close()
+
+
+print("\nSetup is complete. Thank you for using pyMessageServer!")
+print("Please run 'bash run-pyMessageBridge+noJared.sh' to start.")
